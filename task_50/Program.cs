7e04eb4d84cd16be4bfe8c07d82c8c54fbc5fd7c
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-22 16:38:29 +0700

    50

---
+++ task_50/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите позицию строки: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию столбца: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[7, 7];
+ImputMatrix(matrix);
+
+void ImputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 100);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+if (a < matrix.GetLength(0) && b < matrix.GetLength(1)) 
+Console.WriteLine($"Позиции со стракой {a} и столбом {b} -> {matrix[a,b]}.");
+else 
+Console.WriteLine($"Позиции со стракой {a} и столбцом {b} -> в массиве нет");
